47b46eb90ffb7ae3bb3674e1bd58c699dcce83bb
Jim Hazell <jim@pragmatechnology.co.uk>
2016-09-11 00:53:10 +0100

    Changed some Strings in TextValidation to be booleans

--- src/main/java/kennedy/ox/ac/uk/TextValidation.java
+++ src/main/java/kennedy/ox/ac/uk/TextValidation.java
@@ -1,5 +1,7 @@
 package kennedy.ox.ac.uk;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 /**
  * Created by vinodkumar on 10/09/2016.
  */
@@ -10,9 +12,9 @@ public class TextValidation extends Validation  {
 
     String matches = null;
 
-    String valid_url  = null;
-    String valid_email  = null;
-    String valid_emails  = null;
-    String valid_ip  = null;
+    boolean valid_url  = false;
+    boolean valid_email  = false;
+    boolean valid_emails  = false;
+    boolean valid_ip  = false;
 
 }
