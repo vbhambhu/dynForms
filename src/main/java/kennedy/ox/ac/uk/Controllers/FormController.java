b0384a4934cccead807b24ab63ad83b9098193b2
vinodkumar <v.bhambhu083@gmail.com>
2016-09-10 22:27:57 +0100

    Added some CSS and form page

---
+++ src/main/java/kennedy/ox/ac/uk/Controllers/FormController.java
@@ -0,0 +1,33 @@
+package kennedy.ox.ac.uk.Controllers;
+
+import kennedy.ox.ac.uk.Form;
+import kennedy.ox.ac.uk.FormRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+/**
+ * Created by vinodkumar on 10/09/2016.
+ */
+@Controller
+public class FormController {
+
+    @Autowired
+    private FormRepository formRepository;
+
+
+    @RequestMapping(value="/forms", method= RequestMethod.GET)
+    public String showHome(Model model) {
+
+        List<Form> forms = formRepository.findAll();
+        model.addAttribute("forms", forms);
+        return "form/list";
+
+    }
+
+
+}
