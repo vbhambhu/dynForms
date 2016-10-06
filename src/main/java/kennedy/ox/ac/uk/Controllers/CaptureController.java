65769f01271a14aa6dc7ba9e0db4b5c398854a94
Vinod Kumar <v.bhambhu083@gmail.com>
2016-10-06 16:48:48 +0100

    Update CaptureController.java

--- src/main/java/kennedy/ox/ac/uk/Controllers/CaptureController.java
+++ src/main/java/kennedy/ox/ac/uk/Controllers/CaptureController.java
@@ -12,11 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringJoiner;
 
 /**
  * Created by vinod on 08/09/2016.
@@ -33,12 +36,64 @@ public class CaptureController {
 
 
     @RequestMapping(value="/capture/{id}", method= RequestMethod.GET)
-    public String capture(Model model, @PathVariable String id ) {
+    public String capture(Model model,
+                          @PathVariable String id,
+                          HttpServletResponse response,
+                          @CookieValue(value="fidCookie", required=false) String fidCookie,
+                          @CookieValue(value="feildIdsCookie", required=false) String feildIdsCookie) {
 
         Form form = formRepository.findById(id);
         model.addAttribute("form", form);
-        return "capture/index";
 
+
+        System.out.println( this.getFieldIdsCookie(form));
+
+        if(fidCookie != null){
+            Cookie formIdcookie = new Cookie("fidCookie", form.getId());
+            Cookie feildIdscookie = new Cookie("feildIdsCookie", this.getFieldIdsCookie(form) );
+            formIdcookie.setMaxAge(360);
+            feildIdscookie.setMaxAge(360);
+            response.addCookie(formIdcookie);
+            response.addCookie(feildIdscookie);
+        }
+
+        model.addAttribute("fieldLimit", 1);
+
+        //Modify field list here
+
+
+       // System.out.println(feildIdsCookie);
+
+
+        //Settings settings = form.getSettings();
+        //System.out.println(form.getSettings());
+        return "form/capture";
+
+    }
+
+/*
+    private List<Field> getDynamicFields(Form form) {
+
+    }
+*/
+    private String getFieldIdsCookie(Form form){
+
+        StringJoiner fieldSeu = new StringJoiner("|");
+
+        for (Field field : form.getFields()) {
+            fieldSeu.add(Integer.toString(field.getFieldId()) );
+        }
+
+
+
+        /*
+        if adaptive - sequence
+
+        else random - random sequence
+
+        else per page - sequence with per page
+         */
+        return fieldSeu.toString();
     }
 
 
@@ -65,8 +120,7 @@ public class CaptureController {
             if(field.getRequired() != null && field.getRequired()){
 
                 // if required
-                if(fieldValue == null || fieldValue.trim().isEmpty() || fieldValue.trim().length() == 0 ){
-                    formSuccess = false;
+                if(fieldValue == null || fieldValue.trim().isEmpty() ){
                     field.setHasError(true);
                     field.setErrMsg(String.format(" The %s field is required." , field.getName()));
                 }
@@ -119,7 +173,7 @@ public class CaptureController {
         }
 
 
-        return "capture/index";
+        return "form/capture";
 
     }
 }
